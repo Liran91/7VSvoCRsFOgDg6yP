1463705cc96619b83abb89efea411e70b31c2cb2
Liran <lirangilad91@gmail.com>
2016-05-18 04:56:08 +0300

    Third Commit
    Changes:
    •Fixed some variable names and enhanced overall readability.
    
    Newly added:
    •Started writing the main logging function.
    
    •Added 'RunLogger' method.
    The method gets a random file name from a list containing file names.
    
    •Added 'GetLoggingRateDuringPhase' method.
    The method calculates the logging rate for a given phase.
    
    •Added 'GenerateRandomLine' method.
    The method generates a string containing random ASCII characters until
    it reaches the designated string length.
    
    •Added 'GenerateLogString' method.
    The method generates a log string as defined in the Interview Assigment
    document guidelines.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@ using System.Text;
 
 namespace LiranGiladHPEInterviewAssigment
 {
+    enum eLogLevel
+    {
+        INFO = 1, DEBUG, ERROR
+    }
+
     class Program
     {
         const float k_DefaultMaxNumOfEventsPerMin = 1000;
@@ -11,6 +16,7 @@ namespace LiranGiladHPEInterviewAssigment
         const float k_DefaultRampUpPeriodInMin = 0.2f;
         const float k_DefaultTearDownPeriodInMin = 0.2f;
         const int k_NoRangeLimit = 0;
+        Random rand = new Random();
 
         public void Main()
         {
@@ -24,21 +30,80 @@ namespace LiranGiladHPEInterviewAssigment
             float rampUpPeriodInMin = CheckIfInputIsValid(tokenizedInputStr[2], 0, 15, k_DefaultRampUpPeriodInMin);
             float tearDownPeriodInMin = CheckIfInputIsValid(tokenizedInputStr[3], 0, 15, k_DefaultTearDownPeriodInMin);
 
-            int remainingStringsInInputStr = tokenizedInputStr.Length - 4;
+            int remainingStringsInStrArr = tokenizedInputStr.Length - 4;
 
-            for (int i = 4; i < remainingStringsInInputStr; i++)
+            for (int i = 4; i < remainingStringsInStrArr; i++)
             {
                 logFilesNameList.Add(tokenizedInputStr[i]);
             }
 
             CheckIfFileExtensionsAreValid(logFilesNameList);
 
+            string logFileName;
+
+            if (logFilesNameList.Count > 1)
+            {
+                logFileName = GetRandomFileNameFromFileNameList(logFilesNameList);
+            }
+            else
+            {
+                logFileName = logFilesNameList[0];
+            }
+
             RunLogger(MaxNumOfEventsPerMin, loadTestLenInMin, rampUpPeriodInMin, tearDownPeriodInMin, logFilesNameList);
         }
 
-        public void RunLogger(float MaxNumOfEventsPerMin, float loadTestLenInMin, float rampUpPeriodInMin, float tearDownPeriodInMin, List<string> logFileNameList )
-        {
 
+        public void RunLogger(float maxNumOfEventsPerMin, float loadTestLenInMin, float rampUpPeriodInMin, float tearDownPeriodInMin, List<string> logFileNameList )
+        {
+            float rampUpLoggingRate = GetLoggingRateDuringPhase(maxNumOfEventsPerMin, rampUpPeriodInMin);
+            float loadTestLoggingRate = GetLoggingRateDuringPhase(maxNumOfEventsPerMin, loadTestLenInMin);
+            float tearDownLoggingRate = GetLoggingRateDuringPhase(maxNumOfEventsPerMin, tearDownPeriodInMin);
+        }
+
+        public string GenerateLogString()
+        {
+            StringBuilder logString = new StringBuilder();
+
+            logString.Append(DateTime.Now.ToString());
+            logString.Append(" ");
+            eLogLevel logLevel = (eLogLevel)rand.Next(1,3);
+            string logLevelStr = Enum.GetName(typeof(eLogLevel),logLevel);
+
+            logString.Append(logLevelStr);
+            logString.Append(" ");
+            string line1 = GenerateRandomLine(128);
+
+
+        }
+
+        public string GenerateRandomLine(int lineLength)
+        {
+            char ch;
+            StringBuilder randomLine = new StringBuilder();
+
+            for (int i = 0; i < lineLength; i++)
+            {
+                ch = (char)rand.Next(0, 255);
+                randomLine.Append(ch);
+            }
+
+            return (randomLine.ToString());
+        }
+
+        public float GetLoggingRateDuringPhase(float maxNumOfEventsPerMin, float phaseDurationInMin)
+        {
+            float phaseDurationInSecs = phaseDurationInMin * 60;
+            float loggingRate = phaseDurationInSecs / maxNumOfEventsPerMin;
+
+            return loggingRate;
+        }
+
+        public string GetRandomFileNameFromFileNameList(List<string> logFilesNameList)
+        {
+            int fileNameIndex = rand.Next(0, logFilesNameList.Count - 1);
+
+            return (logFilesNameList[fileNameIndex]);
         }
 
         public void CheckIfFileExtensionsAreValid(List<string> logFileNameList)
